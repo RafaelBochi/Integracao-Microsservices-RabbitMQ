3159329159cf73a71ba3a96cde3e2575fcf4713c
RafaelBochi <rafaelbochibarros@gmail.com>
2023-07-30 10:19:49 -0300

    Todo_List: Create( - App user and settings )

--- todo_list/back/config/urls.py
+++ todo_list/back/config/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
-
 from todo_list.views import ListViewSet
+from usuario.router import router as usuario_router
 
 router = DefaultRouter()
 router.register(r"lists", ListViewSet)
@@ -11,4 +11,5 @@ router.register(r"lists", ListViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path("api/", include(usuario_router.urls)),
 ]
