aec7dc516ba7b35870e102672f2f7658b28c2619
RafaelBochi <rafaelbochibarros@gmail.com>
2023-07-30 19:40:45 -0300

    Users: Create App and clean project

--- users/front/src/views/HomeView.vue
+++ users/front/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    home
 </template>
+
+<style scoped>
+
+</style>
