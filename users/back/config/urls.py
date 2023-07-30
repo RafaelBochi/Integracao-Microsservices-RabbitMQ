3b3b7e87ac9a2c8b10ee50a830140b4e40ecde01
RafaelBochi <rafaelbochibarros@gmail.com>
2023-07-30 20:29:35 -0300

    User: Back( - Add url user )

--- users/back/config/urls.py
+++ users/back/config/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from users.views import UserViewSet
+
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register(r"usuarios", UserViewSet)
 
 urlpatterns = [
+    path("", include(router.urls)),
     path('admin/', admin.site.urls),
 ]
 
