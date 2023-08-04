0414105182eedcca0b2d8fddedf130949c9af1c6
Hiansdt <vinijao123690@gmail.com>
2023-08-04 11:49:35 -0300

    Front-end for Todo app
    - Inputs for username and password

---
+++ todo_list/front/src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<script>
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Página Login - Teste RabbitMQ</h1>
+
+    <div class="inputs">
+
+      <input type="text" placeholder="Username">
+      <input type="text" placeholder="Password">
+
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin: 20px;
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: 100;
+}
+
+.inputs {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+input {
+  margin: 10px;
+  padding: 10px;
+  width: 300px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
+</style>
